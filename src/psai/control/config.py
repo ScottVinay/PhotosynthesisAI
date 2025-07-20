2227ffa86bdb78b2ddcf0a45b8217d7c3e8e746a
Scott <scott.vinay.93@gmail.com>
2025-07-20 18:08:31 +0100

    Added config

---
+++ src/psai/control/config.py
@@ -0,0 +1,23 @@
+import json
+from dataclasses import dataclass
+from typing import Optional
+import importlib.resources as pkg_resources
+from psai import control  # this points to the folder containing config.json
+
+@dataclass
+class ControlConfig:
+    sun_stays_at_zero: bool
+
+# Singleton-ish instance cache
+_config_instance: ControlConfig | None = None
+
+def load_config(name: Optional[str]=None) -> ControlConfig:
+    global _config_instance
+    if _config_instance is None:
+        if name is None:
+            name = "default"
+        config_path = pkg_resources.files(control).joinpath(f"configs/{name}.json")
+        with config_path.open("r", encoding="utf-8") as f:
+            raw = json.load(f)
+        _config_instance = ControlConfig(**raw)
+    return _config_instance
