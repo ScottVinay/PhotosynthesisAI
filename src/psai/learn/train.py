f3795c71b828c2b5292ae9dd3e662beeb7ead796
Scott <scott.vinay.93@gmail.com>
2025-09-27 21:22:26 +0100

    Masked PPO in train

--- src/psai/learn/train.py
+++ src/psai/learn/train.py
@@ -1,9 +1,13 @@
 print('import logging')
-import logging
+import logging; from pprint import pprint
 print('from psai.game.env import PhotosynthesisEnv, MultiplayerEnvWrapper')
-from psai.game.env import PhotosynthesisEnv, MultiplayerEnvWrapper
+from psai.game.env import PhotosynthesisEnv, build_multiplayer_env
+from psai.agents import RandomAgent
 print('from stable_baselines3 import PPO')
-from stable_baselines3 import PPO
+from sb3_contrib import MaskablePPO
+from sb3_contrib.common.wrappers import ActionMasker
+import numpy as np
+from pprint import pprint
 
 # Configure logging
 logging.basicConfig(level=logging.INFO)
@@ -11,11 +15,23 @@ logger = logging.getLogger(__name__)
 
 # 1. Create environment
 logger.info("Creating PhotosynthesisEnv environment...")
-env = PhotosynthesisEnv(render_mode='matplotlib')
+multiplayer_env = build_multiplayer_env(
+    PhotosynthesisEnv,
+    agents=(
+        None,
+        RandomAgent(),
+        RandomAgent(),
+        RandomAgent()
+    ),
+    render_mode='matplotlib',
+)
+
+#TODO why env masker?
+masked_env = ActionMasker(multiplayer_env, "get_action_mask") # The string is the method name in the env that returns the mask
 
 # 2. Initialize PPO agent
 logger.info("Initializing PPO agent...")
-model = PPO("MlpPolicy", env, verbose=1)
+model = MaskablePPO("MlpPolicy", masked_env, verbose=1)
 
 # 3. Train the agent
 logger.info("Starting training for 10000 timesteps...")
