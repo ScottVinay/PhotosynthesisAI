905392cafa47eec214f6d853ce9f17ea0e59589d
Scott <scott.vinay.93@gmail.com>
2025-09-28 13:31:58 +0100

    Added distribute_light function

--- src/psai/game/update.py
+++ src/psai/game/update.py
@@ -5,10 +5,8 @@ File for the main function for updating the state of the game.
 from psai.game.assertions import assertion_loc
 from psai.game.objects import State, Cell, Action, PlayerStats
 from psai.game.actions import get_purchase_cost
-from psai.game.utils import get_score_from_n_score_cards_taken
-from psai.game.utils import rotate_board
+from psai.game.utils import get_score_from_n_score_cards_taken, rotate_board, get_shaded_locs
 from psai.control.config import load_config
-
 from pprint import pprint
 
 def update_state(
@@ -156,17 +154,16 @@ def update_state(
         state.active_player = (state.active_player + 1) % 4
 
         # Distribute light if all players have played
-        distribute_light(state)
 
-        if state.active_player == 0:
+        if state.active_player == 0: #TODO change this, the "first player" rotates each hour. Another variable?
+            distribute_light(state)
             # Rotate the sun position if all players have gone.
             state.sun_pos = (state.sun_pos + 1) % 6
             # Also update the hour (same as sun_pos)
             state.hour = (state.hour + 1) % 6
 
-            # Increment the turn counter if the sun position is at 0
+            # Increment the day counter if the sun position is at 0
             if state.sun_pos == 0:
-                # Reset the used status for all cells
                 state.day += 1
         
         # Reset used status for all cells
@@ -193,5 +190,38 @@ def distribute_light(state: State) -> None:
     state : State
         The current game state.
     """
-    ...
-    #TODO complete
+    for cell in state.board_state:
+        if cell.size is not None and cell.owner is not None:
+            # Determine the light received based on tree size
+            if cell.size == 'seed':
+                light_received = 0
+            elif cell.size == 'small':
+                light_received = 1
+            elif cell.size == 'medium':
+                light_received = 2
+            elif cell.size == 'large':
+                light_received = 3
+            else:
+                raise ValueError(f"Unknown tree size: {cell.size}")
+
+            # Check if the tree is shaded
+            # We check the cells in the direction of the sun as the shaded locations 
+            # if the sun was in the opposite direction (state.sun_pos+3) % 6.
+            shaded_locs = get_shaded_locs(cell.loc[0], cell.loc[1], (state.sun_pos+3) % 6)
+            is_shaded = False
+            shading_sizes = {
+                1: ['small', 'medium', 'large'],
+                2: ['medium', 'large'],
+                3: ['large']
+            }
+            for iloc, loc in enumerate(shaded_locs, start=1):
+                if loc is not None:
+                    shader_cell = state.get_cell_at_loc(loc)
+                    if shader_cell.size is None:
+                        continue
+                    if shader_cell.size in shading_sizes[iloc]:
+                        is_shaded = True
+                        break
+
+            if not is_shaded:
+                state.player_stats[cell.owner].light += light_received
