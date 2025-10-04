608583c477eb8e39bb5a26e0afbd64ecb08c86c9
Scott <scott.vinay.93@gmail.com>
2025-10-04 11:35:19 +0100

    Train updated to many rounds

--- src/psai/learn/train.py
+++ src/psai/learn/train.py
@@ -2,58 +2,95 @@ print('import logging')
 import logging; from pprint import pprint
 print('from psai.game.env import PhotosynthesisEnv, MultiplayerEnvWrapper')
 from psai.game.env import PhotosynthesisEnv, build_multiplayer_env
-from psai.agents import RandomAgent, FrozenSB3  
+from psai.agents import RandomAgent, FrozenSB3, BaseAgent
 print('from stable_baselines3 import PPO')
 from sb3_contrib import MaskablePPO
 from sb3_contrib.common.wrappers import ActionMasker
 from stable_baselines3.common.monitor import Monitor
+from stable_baselines3.common.monitor import load_results
 from typing import Optional, Dict, Union, Tuple, List, cast
 
 import numpy as np
 from pprint import pprint
+import os
+import uuid
+from datetime import datetime
 
 # Configure logging
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 
-logfolder = '/Users/scott/My Drive/Github/PhotosynthesisAI/logs/'
+# 0. Set folders
+base_logs_folder = '/Users/scott/My Drive/Github/PhotosynthesisAI/logs/'
+base_models_folder = '/Users/scott/My Drive/Github/PhotosynthesisAI/model_files/'
 
-# 1. Create environment
-logger.info("Creating PhotosynthesisEnv environment...")
-multiplayer_env = build_multiplayer_env(
-    PhotosynthesisEnv,
-    agents=(
-        None,
-        RandomAgent(),
-        RandomAgent(),
-        RandomAgent(),
-        # FrozenSB3('/Users/scott/My Drive/Github/PhotosynthesisAI/model_files/1'),
-        # FrozenSB3('/Users/scott/My Drive/Github/PhotosynthesisAI/model_files/1'),
-        # FrozenSB3('/Users/scott/My Drive/Github/PhotosynthesisAI/model_files/1'),
-    ),
-    render_mode='matplotlib',
-)
-masked_env = ActionMasker(multiplayer_env, "get_action_mask") # The string is the method name in the env that returns the mask
-monitored_env = Monitor(masked_env, filename = logfolder + "monitor.csv")
+current_time = datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
+logs_folder = os.path.join(base_logs_folder, f"run_{current_time}/")
+models_folder = os.path.join(base_models_folder, f"run_{current_time}/")
 
-# 2. Initialize PPO agent
-logger.info("Initializing PPO agent...")
-model = MaskablePPO("MlpPolicy", monitored_env, verbose=1)
-# model = MaskablePPO.load('/Users/scott/My Drive/Github/PhotosynthesisAI/model_files/1', env=monitored_env)
+os.makedirs(logs_folder, exist_ok=True)
+os.makedirs(models_folder, exist_ok=True)
 
-# 3. Train the agent
-logger.info("Starting training...")
-model.learn(total_timesteps=100_000)
-logger.info("Training complete.")
+def play_round(
+        initial_model_path: Optional[str] = None,
+        training_idx: int = 0,
+        logs_folder: str = '',
+    ) -> None:
+    logs_subfolder = os.path.join(logs_folder, f"round_{training_idx}/")
 
-from stable_baselines3.common.monitor import load_results
-results = load_results(logfolder)   # 1 row per finished episode
+    # 1. Create environment
+    logger.info("Creating PhotosynthesisEnv environment...")
 
-episode_rewards = (results['r']).astype(int).values
+    if initial_model_path is None:
+        agents = (
+            None,
+            RandomAgent(),
+            RandomAgent(),
+            RandomAgent()
+        )
+    else:
+        agents = (
+            None,
+            FrozenSB3(initial_model_path),
+            FrozenSB3(initial_model_path),
+            FrozenSB3(initial_model_path)
+        )
 
-with open(logfolder + "episode_rewards.log", "w") as f:
-    for reward in episode_rewards:
-        f.write(f"{reward}, ")
-logger.info(f"Episode rewards saved to {logfolder}episode_rewards.log")
+    multiplayer_env = build_multiplayer_env(
+        PhotosynthesisEnv,
+        agents=agents,
+        render_mode='matplotlib',
+    )
+    masked_env = ActionMasker(multiplayer_env, "get_action_mask") # The string is the method name in the env that returns the mask
 
-print('done')
+    monitored_env = Monitor(masked_env, filename = logs_subfolder + "monitor.csv")
+
+    # 2. Initialize PPO agent
+    logger.info("Initializing PPO agent...")
+    model = MaskablePPO("MlpPolicy", monitored_env, verbose=1)
+    if initial_model_path is not None:
+        model = MaskablePPO.load(initial_model_path, env=monitored_env)
+
+    # 3. Train the agent
+    logger.info("Starting training...")
+    model.learn(total_timesteps=50_000)
+    logger.info("Training complete.")
+
+    # 4. Save the trained model
+    model.save(models_folder + "trained_model_" + str(training_idx) + ".zip")
+
+
+def play_many_rounds(num_rounds: int = 10) -> None:
+    for i in range(num_rounds):
+        logger.info(f"Starting round {i+1} of {num_rounds}...")
+        play_round(
+            initial_model_path=None if i == 0 else models_folder + "trained_model_" + str(i-1) + ".zip",
+            training_idx=i,
+            logs_folder=logs_folder,
+        )
+        logger.info(f"Round {i+1} complete.")
+
+
+if __name__ == "__main__":
+    play_many_rounds(num_rounds=4)
+    print('done')
