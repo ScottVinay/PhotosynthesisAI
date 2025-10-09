4818eb974b4c9cc96a0dec3a3a6fcf0908e6659e
Scott <scott.vinay.93@gmail.com>
2025-10-09 17:55:33 +0100

    Configs updated

--- src/psai/control/config.py
+++ src/psai/control/config.py
@@ -5,7 +5,7 @@ import importlib.resources as pkg_resources
 from psai import control  # this points to the folder containing config.json
 
 @dataclass
-class ControlConfig:
+class GameConfig:
     sun_stays_at_zero: bool
     reward_method: str
 
@@ -13,16 +13,36 @@ class ControlConfig:
         if self.reward_method not in ["score", "normed", "winner", "winning_score"]:
             raise ValueError(f"Invalid reward_method: {self.reward_method}")
 
-# Singleton-ish instance cache
-_config_instance: ControlConfig | None = None
+@dataclass
+class TrainingConfig:
+    num_rounds: int
+    timesteps_per_round: int
 
-def load_config(name: Optional[str]=None) -> ControlConfig:
-    global _config_instance
-    if _config_instance is None:
+    def __post_init__(self):
+        pass
+
+# Singleton-ish instance cache
+_game_config_instance: GameConfig | None = None
+_training_config_instance: TrainingConfig | None = None
+
+def load_game_config(name: Optional[str]=None) -> GameConfig:
+    global _game_config_instance
+    if _game_config_instance is None:
         if name is None:
             name = "default"
-        config_path = pkg_resources.files(control).joinpath(f"configs/{name}.json")
+        config_path = pkg_resources.files(control).joinpath(f"game_configs/{name}.json")
         with config_path.open("r", encoding="utf-8") as f:
             raw = json.load(f)
-        _config_instance = ControlConfig(**raw)
-    return _config_instance
+        _game_config_instance = GameConfig(**raw)
+    return _game_config_instance
+
+def load_training_config(name: Optional[str]=None) -> TrainingConfig:
+    global _training_config_instance
+    if _training_config_instance is None:
+        if name is None:
+            name = "default"
+        config_path = pkg_resources.files(control).joinpath(f"training_configs/{name}.json")
+        with config_path.open("r", encoding="utf-8") as f:
+            raw = json.load(f)
+        _training_config_instance = TrainingConfig(**raw)
+    return _training_config_instance
